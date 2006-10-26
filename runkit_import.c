d2ebeb125e1e5620b5ac6a483d650f5080fc4d56
pollita <pollita@c90b9560-bf6c-de11-be94-00142212c4b1>
2006-10-26 16:18:52 +0000

    Catch signature change in zend_unmangle_property_name() -- Thanks steph

--- runkit_import.c
+++ runkit_import.c
@@ -228,7 +228,9 @@ static int php_runkit_import_class_props(zend_class_entry *dce, zend_class_entry
 		if (zend_hash_get_current_key_ex(&ce->default_properties, &key, &key_len, &idx, 0, &pos) == HASH_KEY_IS_STRING) {
 			char *cname = NULL, *pname = key;
 
-#ifdef ZEND_ENGINE_2
+#ifdef ZEND_ENGINE_2_2
+			zend_unmangle_property_name(key, key_len - 1, &cname, &pname);
+#elif defined(ZEND_ENGINE_2)
 			zend_unmangle_property_name(key, &cname, &pname);
 #endif
 			if (zend_hash_exists(&dce->default_properties, key, key_len)) {
